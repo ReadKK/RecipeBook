ba36a6752ad743ad41b91167e46bb90d0e9c208a
РАДОСЛАВ КСЕНОФОНТОС <s20621621@onlineedu.tu-varna.bg>
2023-05-03 21:49:00 +0300

    scope_identity!!!

--- RecipeBook/Form2.Designer.cs
+++ RecipeBook/Form2.Designer.cs
@@ -38,6 +38,12 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.textBox4 = new System.Windows.Forms.TextBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // comboBox1
@@ -50,7 +56,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(443, 45);
+            this.button1.Location = new System.Drawing.Point(703, 45);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
@@ -117,16 +123,75 @@
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(688, 398);
+            this.textBox4.Location = new System.Drawing.Point(536, 108);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.Size = new System.Drawing.Size(90, 20);
             this.textBox4.TabIndex = 9;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(384, 45);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 10;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(384, 108);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 11;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(536, 45);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(131, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(381, 26);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(381, 92);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(632, 108);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "label7";
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -157,5 +222,11 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
     }
 }
--- RecipeBook/Form2.cs
+++ RecipeBook/Form2.cs
@@ -21,11 +21,17 @@ namespace RecipeBook
             label2.Text = "Description";
             label3.Text = "Instructions";
             label4.Text = "Dish type";
-            button1.Text = "Insert";
+            label5.Text = "Ingredients";
+            label6.Text = "Ing.id/Name/quantity";
+            label7.Text = "grams/milliliters";
+            button1.Text = "Add recipe";
+            button2.Text = "Add to the list";
         }
         public string cs = @"Data Source=(LocalDB)\mssqllocaldb;AttachDbFilename=C:\Users\user\C#\RecipeBook\RecipeBook\Recipe.mdf;Integrated Security=True";
         public SqlConnection myConnection = default(SqlConnection);
         public SqlCommand myCommand = default(SqlCommand);
+        public SqlCommand avIng = default(SqlCommand);
+        
 
         private void Form2_Load(object sender, EventArgs e)
         {
@@ -33,15 +39,30 @@ namespace RecipeBook
 
             myConnection.Open();
             myCommand = new SqlCommand("Select * from dish_type", myConnection);
-            SqlDataAdapter dataAdapter = new SqlDataAdapter(myCommand);
-            DataSet dataSet = new DataSet();
-            dataAdapter.Fill(dataSet);
+            SqlDataAdapter dataAdapter1 = new SqlDataAdapter(myCommand);
+            DataSet dataSet1 = new DataSet();
+            dataAdapter1.Fill(dataSet1);
             myCommand.ExecuteNonQuery();
             myConnection.Close();
 
-            comboBox1.DataSource = dataSet.Tables[0];
             comboBox1.DisplayMember = "dish_name";
             comboBox1.ValueMember = "dish_id";
+            comboBox1.DataSource = dataSet1.Tables[0];
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            
+
+            myConnection.Open();
+            myCommand = new SqlCommand("Select * from ingredients", myConnection);
+            SqlDataAdapter dataAdapter2 = new SqlDataAdapter(myCommand);
+            DataSet dataSet2 = new DataSet();
+            dataAdapter2.Fill(dataSet2);
+            myCommand.ExecuteNonQuery();
+            myConnection.Close();
+
+            comboBox2.DisplayMember = "ingredient_name";
+            comboBox2.ValueMember = "ingredient_id";
+            comboBox2.DataSource = dataSet2.Tables[0];
+            comboBox2.DropDownStyle = ComboBoxStyle.DropDownList;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -49,25 +70,26 @@ namespace RecipeBook
             try
             {
                 myConnection = new SqlConnection(cs);
-                myCommand = new SqlCommand("INSERT INTO recipe ( recipe_name,recipe_desc,recipe_instruct, dish_id) VALUES ( @recipe_name,@recipe_desc,@recipe_instruct, @dish_id)", myConnection);
+                //myCommand = new SqlCommand("INSERT INTO recipe ( recipe_name,recipe_desc,recipe_instruct, dish_id) VALUES ( @recipe_name,@recipe_desc,@recipe_instruct, @dish_id)", myConnection);
+                avIng = new SqlCommand("INSERT INTO available_ingredients ( avl_ingredient_quantity,recipe_id, ingredient_id) VALUES ( @b,@c, @d) ", myConnection);
+                
+                string sql = @"INSERT INTO  recipe ( recipe_name,recipe_desc,recipe_instruct, dish_id) VALUES ( @recipe_name,@recipe_desc,@recipe_instruct, @dish_id);SELECT Scope_Identity()";
+                SqlCommand myCommand= new SqlCommand(sql,myConnection);
+                
+                
+
                 myConnection.Open();
                 myCommand.Parameters.AddWithValue("@recipe_name", textBox1.Text);
                 myCommand.Parameters.AddWithValue("@recipe_desc", textBox2.Text);
                 myCommand.Parameters.AddWithValue("@recipe_instruct", textBox3.Text);
-                if (comboBox1.SelectedIndex == 0)
-                {
-                    myCommand.Parameters.AddWithValue("@dish_id", 1);
-                }
-                else if (comboBox1.SelectedIndex == 1)
-                {
-                    myCommand.Parameters.AddWithValue("@dish_id", 2);
-                }
-                else if (comboBox1.SelectedIndex == 2)
-                {
-                    myCommand.Parameters.AddWithValue("@dish_id", 3);
-                }
-                else { myCommand.Parameters.AddWithValue("@dish_id", 4); }
-                myCommand.ExecuteNonQuery();
+                myCommand.Parameters.AddWithValue("@dish_id", comboBox1.SelectedValue);
+
+                int newID = (int)(decimal)myCommand.ExecuteScalar();
+
+                avIng.Parameters.AddWithValue("@b", textBox4.Text);
+                avIng.Parameters.AddWithValue("@c", newID);
+                avIng.Parameters.AddWithValue("@d", comboBox2.SelectedValue);
+                avIng.ExecuteNonQuery();
                 myConnection.Close();
 
                 MessageBox.Show("Insert successful!");
@@ -82,6 +104,18 @@ namespace RecipeBook
             }
         }
 
-
+        private void button2_Click(object sender, EventArgs e)
+        {
+            decimal d;
+            if (decimal.TryParse(textBox4.Text, out d))
+            {
+                string s =comboBox2.SelectedValue+"; "+ comboBox2.Text + "; " + textBox4.Text;
+                listBox1.Items.Add(s);
+            }
+            else {
+                MessageBox.Show("Please enter a valid number");
+                return;
+            }
+        }
     }
 }
